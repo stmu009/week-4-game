7f4831912bb01f36eac9dd22ba336c7adfa2ea5b
Damian <dramirez@petmate.com>
2018-03-15 20:31:37 -0500

    comments for object pseudocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+//create object for the game
+//add characters
+    //add health points
+    //add attack power
+    //add counter attack power
+
+//add attacker
+//add defender
+
+//method for attack
+//method for counter
+//method to increase attack
+//method to decrease health
+
+//add death state
+//add kill state
+
+//add win state
