59ce4e440fc081ffb2c909b437eabd8b2305b938
Damian <dramirez@petmate.com>
2018-03-19 23:12:43 -0500

    added select defender logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,3 @@
-
 /* Move to defense section
 $(document).on("click", ".enemies-available", function() {
   for (i = 0; i < charactersAvailable.length; i++) {
@@ -38,59 +37,82 @@ var game = {
       health: 156
     }
   },
-  showCharactersAvailable: function() {
-    $.each(game.charactersAvailable, function(key, value) {
+  showCharactersAvailable: function () {
+    $.each(game.charactersAvailable, function (key, value) {
       $(".characters-section").append(
         '<div id="' +
-          key +
-          '" class="characters-available">' +
-          game.charactersAvailable[key].displayName +
-          "</div>"
+        key +
+        '" class="characters-available">' +
+        game.charactersAvailable[key].displayName +
+        "</div>"
       );
     });
   },
-  selectCharacter: function() {
-    $(".characters-available").on("click", function() {
-        selectedCharacter = this;
-      $.each(game.charactersAvailable, function(key, value) {
+  selectCharacter: function () {
+    $(".characters-available").on("click", function () {
+      selectedCharacter = this;
+      $.each(game.charactersAvailable, function (key, value) {
         if (key == selectedCharacter.id) {
           $("#your-character-title").after(
             '<div id="' +
-              key +
-              '" >' +
-              game.charactersAvailable[key].displayName +
-              "</div>"
+            key +
+            '" >' +
+            game.charactersAvailable[key].displayName +
+            "</div>"
           );
         } else {
           $("#enemies-available-title").after(
             '<div class="enemies-available" id="' +
-              key +
-              '">' +
-              game.charactersAvailable[key].displayName +
-              "</div>"
+            key +
+            '">' +
+            game.charactersAvailable[key].displayName +
+            "</div>"
           );
         }
       });
-      game.attacker.attackerID= selectedCharacter.id
+      game.attacker.attackerID = selectedCharacter.id
       game.hideCharactersAvailable()
+
     });
   },
-  hideCharactersAvailable: function() {
-      $('.characters-section').hide();
+  hideCharactersAvailable: function () {
+    $('.characters-section').hide();
+
   },
-  attacker: { alive: true },
-  enemiesAvailable: function() {},
-  defender: { alive: true },
-  attack: function() {},
-  counter: function() {},
-  increaseAttack: function() {},
-  decreaseHealth: function() {},
-  updateStats: function() {},
+  attacker: {
+    alive: true
+  },
+  selectDefender: function () {
+    $(document).on("click", ".enemies-available", function () {
+      selectedDefender = this
+      $.each(game.charactersAvailable, function (key, value) {
+        if (key == selectedDefender.id) {
+          game.defender.defenderID = selectedDefender.id
+          $(".defense-section").append(
+            '<div class="" id="">' + game.charactersAvailable[key].displayName + "</div>"
+          );
+        }
+        game.removeDefenderSelected(selectedDefender.id)
+      })
+    })
+  },
+  removeDefenderSelected: function (key) {
+    $('#' + key).remove()
+  },
+  defender: {
+    alive: true
+  },
+  attack: function () {},
+  counter: function () {},
+  increaseAttack: function () {},
+  decreaseHealth: function () {},
+  updateStats: function () {},
   wins: 0,
   losses: 0,
-  restart: function() {}
+  restart: function () {}
 };
-$(document).ready(function() {
+$(document).ready(function () {
   game.showCharactersAvailable();
   game.selectCharacter();
+  game.selectDefender()
 });
